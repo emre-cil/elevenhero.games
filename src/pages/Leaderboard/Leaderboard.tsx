e4e18ee2429be5480498dcadd530f67482e01081
emre cil <info.emrecil@gmail.com>
2023-03-30 17:37:30 +0300

    feat: scoreboard list

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,10 +1,54 @@
 import React, { useState } from 'react';
-import { Container, Stack, Typography } from '@mui/material';
+import { Container, Stack, Typography, styled } from '@mui/material';
 import LeaderBoardCard from '../../components/Cards/LeaderBoardCard';
 
 function Leaderboard() {
   // monthly 1, all time 2
   const [selectedBoard, setSelectedBoard] = useState(1);
+
+  const scores = [
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+    {
+      name: 'John Doe',
+      score: 100,
+    },
+  ];
+
   return (
     <Container
       sx={{
@@ -51,17 +95,34 @@ function Leaderboard() {
           All Time
         </Typography>
       </Stack>
-      <Stack
-        direction="row"
-        gap={2}
-        justifyContent="center"
-        sx={{
-          height: '500px',
-        }}
-      >
+      <Stack direction="row" gap={2} justifyContent="center" sx={{ mb: '1rem' }}>
         <LeaderBoardCard color="Card.Silver" mt={2} />
         <LeaderBoardCard color="Card.Gold" />
         <LeaderBoardCard color="Card.Bronze" mt={2.25} />
+      </Stack>
+      <Stack gap={2}>
+        {scores.map((score, index) => (
+          <Stack
+            direction="row"
+            sx={{
+              justifyContent: 'space-between',
+              borderRadius: '14px',
+              backgroundColor: 'Ink.Base',
+              p: '1rem',
+            }}
+            key={index}
+          >
+            <Typography variant="body2" component="h3" color="white">
+              {index + 1}
+            </Typography>
+            <Typography variant="body2" component="h3" color="white">
+              {score.name}
+            </Typography>
+            <Typography variant="body2" component="h3" color="white">
+              {score.score}
+            </Typography>
+          </Stack>
+        ))}
       </Stack>
     </Container>
   );

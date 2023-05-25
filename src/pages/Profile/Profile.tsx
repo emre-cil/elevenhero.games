975f8a224e9c9e9516d41774aa7df7964125d22b
emre cil <info.emrecil@gmail.com>
2023-05-25 13:02:59 +0300

    feat: add input for image

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Container, Link, Stack } from '@mui/material';
+import { Box, Button, Container, Link, Stack } from '@mui/material';
 import ConnectWallet from '@/components/ConnectWallet';
 import { useAppDispatch } from '@/app/store';
 import { logout } from '@/features/user/userSlice';
@@ -8,6 +8,8 @@ import Loading from '@/components/Loading';
 
 function Profile() {
   const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
+  const [imageSrc, setImageSrc] = useState<any>();
+  const [selectedImage, setSelectedImage] = useState<any>(null);
   const dispatch = useAppDispatch();
   const [logoutF] = useLazyLogoutQuery();
   const { data: details, isLoading } = useGetDetailsQuery(undefined);
@@ -15,6 +17,55 @@ function Profile() {
 
   console.log(details);
 
+  const handleOperation = () => {
+    //     .then((res: any) => {
+    //       if (res?.data?.message) {
+    //         toast.success(res?.data?.message);
+    //         handleClear();
+    //       }
+    //       if (res?.error) {
+    //         toast.error('Update product failed');
+    //       }
+    //     })
+    //     .catch(() => {
+    //       toast.error('Update product failed');
+    //     });
+    // } else {
+    //   const formData = new FormData();
+    //   formData.append('title', Title);
+    //   formData.append('price', Price.toString());
+    //   formData.append('description', Description);
+    //   formData.append('image', selectedImage);
+    //   formData.append('isActive', isActive.toString());
+    //   serie && formData.append('serie', serie._id);
+    //   createObject(formData)
+    //     .then((res: any) => {
+    //       if (res?.error) {
+    //         toast.error('Add product failed');
+    //       } else {
+    //         toast.success('Add product successfully');
+    //         handleClear();
+    //       }
+    //     })
+    //     .catch(() => {
+    //       toast.error('Add product failed');
+    //     });
+    // }
+    console.log(selectedImage);
+    const formData = new FormData();
+    formData.append('image', selectedImage);
+
+    updateImage({
+      data: formData,
+    })
+      .unwrap()
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   // wins, losses, draws, monthlyScore, totalScore, username, walletAddress, image
   const logoutHandler = () => {
     dispatch(logout());
@@ -24,6 +75,59 @@ function Profile() {
     <Container maxWidth="md" sx={{ py: 3 }}>
       <Stack>
         <Loading loading={isLoading} />
+        <Box
+          sx={{
+            display: 'flex',
+            '& .file-dropzone': {
+              p: 1.5,
+              borderRadius: '50%',
+              width: 200,
+              height: 200,
+              border: '1px dashed #ccc',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              cursor: 'pointer',
+              '&:hover': {
+                backgroundColor: '#f5f5f5',
+              },
+            },
+            '& .file-input': {
+              display: 'none',
+            },
+          }}
+        >
+          <label htmlFor="image" className="file-dropzone">
+            <img
+              src={imageSrc ? imageSrc : `${import.meta.env.VITE_API_URL}/uploads/${details?.image}`}
+              alt="profile"
+              style={{
+                height: '100%',
+                maxWidth: '100%',
+                objectFit: 'cover',
+              }}
+            />
+
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              onChange={(e: any) => {
+                if (e.target.files[0]) {
+                  setSelectedImage(e.target.files[0]);
+                  const reader = new FileReader();
+                  reader.onload = () => {
+                    const imageSrc = reader.result as string;
+                    setImageSrc(imageSrc);
+                  };
+                  reader.readAsDataURL(e.target.files[0]);
+                }
+                handleOperation();
+              }}
+              className="file-input"
+            />
+          </label>
+        </Box>
         <Button variant="contained" onClick={() => setIsWalletModalOpen(true)}>
           Connect Wallet
         </Button>

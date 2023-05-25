e0792d35b1a109dca46a3775257d07813347b22b
emre cil <info.emrecil@gmail.com>
2023-05-25 10:13:40 +0300

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import store from './app/store';
 import AppThemeProvider from './themes/AppThemeProvider';
 import App from './App';
 import './main.css';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-3E4ET9ZSHF');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>

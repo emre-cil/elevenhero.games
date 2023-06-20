ead27b44b8f18f6479950aecb053cbf8adad193d
Emre Çil <65237072+emre-cil@users.noreply.github.com>
2023-06-20 22:57:37 +0300

    feat: update balance card

--- src/components/Cards/BalanceCard.tsx
+++ src/components/Cards/BalanceCard.tsx
@@ -1,6 +1,5 @@
-import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Stack, Typography, Box, CircularProgress } from '@mui/material';
+import { FC, useState } from 'react';
+import { Stack, Typography, Box, CircularProgress, Modal, Tabs, Tab } from '@mui/material';
 import walletIcon from '@/assets/Icons/walletIcon.svg';
 import { useGetMoneyQuery } from '@/features/user/userApiSlice';
 
@@ -9,9 +8,20 @@ type BalanceCardProps = {
 };
 
 const BalanceCard: FC<BalanceCardProps> = ({ accessToken }) => {
-  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState<number | null>(null);
+  console.log(isOpen);
   const { data: money, isLoading } = useGetMoneyQuery(undefined);
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log(newValue);
+    setIsOpen(newValue);
+  };
   return (
     <Stack
       direction="row"
@@ -24,7 +34,10 @@ const BalanceCard: FC<BalanceCardProps> = ({ accessToken }) => {
         backgroundColor: 'Ink.Dark',
         cursor: 'pointer',
       }}
-      onClick={() => navigate('/profile')}
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsOpen(1012);
+      }}
     >
       {accessToken ? (
         <>
@@ -68,6 +81,38 @@ const BalanceCard: FC<BalanceCardProps> = ({ accessToken }) => {
           PLAY
         </Typography>
       )}
+      <>
+        {isOpen !== null && (
+          <Modal
+            open={isOpen !== null}
+            onClose={() => setIsOpen(13)}
+            sx={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              zIndex: 99999,
+            }}
+          >
+            <Stack
+              sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: 'grey.200',
+                borderRadius: '8px',
+              }}
+            >
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                {/* <Tabs value={isOpen} onChange={handleChange} aria-label="basic tabs example">
+                  <Tab label="Deposit" {...a11yProps(0)} />
+                  <Tab label="Withdraw" {...a11yProps(1)} />
+                </Tabs> */}
+              </Box>
+            </Stack>
+          </Modal>
+        )}
+      </>
     </Stack>
   );
 };

90bbbb52293986ba492ca34574134d916153f017
emre-cil <info.emrecil@gmail.com>
2023-06-21 17:48:05 +0300

    fix: deposit modal

--- src/components/Cards/BalanceCard.tsx
+++ src/components/Cards/BalanceCard.tsx
@@ -8,112 +8,130 @@ type BalanceCardProps = {
 };
 
 const BalanceCard: FC<BalanceCardProps> = ({ accessToken }) => {
-  const [isOpen, setIsOpen] = useState<number | null>(null);
-  console.log(isOpen);
-  const { data: money, isLoading } = useGetMoneyQuery(undefined);
-  function a11yProps(index: number) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+  const [isOpen, setIsOpen] = useState<number>(-1);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    console.log(newValue);
+  const { data: money, isLoading } = useGetMoneyQuery(undefined);
+  const a11yProps = (index: any) => {
+    return {
+      id: `scrollable-auto-tab-${index}`,
+      'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    };
+  };
+  const handleChange = (event: any, newValue: any) => {
     setIsOpen(newValue);
   };
   return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      sx={{
-        borderRadius: '8px',
-        overflow: 'hidden',
-        width: '120px',
-        height: '30px',
-        backgroundColor: 'Ink.Dark',
-        cursor: 'pointer',
-      }}
-      onClick={(e) => {
-        e.stopPropagation();
-        setIsOpen(1012);
-      }}
-    >
-      {accessToken ? (
-        <>
-          <Box
-            sx={{
-              backgroundColor: 'Green.Base',
-              height: '100%',
-              width: '40px',
-              p: '6px',
-              img: {
-                width: 20,
-              },
-            }}
-          >
-            <img src={walletIcon} alt="wallet icon" />
-          </Box>
-          {isLoading ? (
-            <CircularProgress size={16} sx={{ mx: 'auto' }} />
-          ) : (
-            <Typography
-              variant="h6"
+    <>
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{
+          borderRadius: '8px',
+          overflow: 'hidden',
+          width: '120px',
+          height: '30px',
+          backgroundColor: 'Ink.Dark',
+          cursor: 'pointer',
+        }}
+        onClick={() => {
+          setIsOpen(0);
+        }}
+      >
+        {accessToken ? (
+          <>
+            <Box
               sx={{
-                color: 'grey.900',
-                textAlign: 'center',
-                width: '100%',
+                backgroundColor: 'Green.Base',
+                height: '100%',
+                width: '40px',
+                p: '6px',
+                img: {
+                  width: 20,
+                },
               }}
             >
-              {money?.money}
-            </Typography>
-          )}
-        </>
-      ) : (
-        <Typography
-          variant="body2"
+              <img src={walletIcon} alt="wallet icon" />
+            </Box>
+            {isLoading ? (
+              <CircularProgress size={16} sx={{ mx: 'auto' }} />
+            ) : (
+              <Typography
+                variant="h6"
+                sx={{
+                  color: 'grey.900',
+                  textAlign: 'center',
+                  width: '100%',
+                }}
+              >
+                {money?.money}
+              </Typography>
+            )}
+          </>
+        ) : (
+          <Typography
+            variant="body2"
+            sx={{
+              color: 'grey.900',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            PLAY
+          </Typography>
+        )}
+      </Stack>
+      <Modal
+        open={isOpen !== -1}
+        onClose={(e: any) => {
+          e.stopPropagation();
+          setIsOpen(-1);
+        }}
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          zIndex: 99999,
+        }}
+      >
+        <Stack
           sx={{
-            color: 'grey.900',
-            textAlign: 'center',
-            width: '100%',
+            width: 300,
+            height: 300,
+            backgroundColor: 'grey.200',
+            borderRadius: '8px',
           }}
         >
-          PLAY
-        </Typography>
-      )}
-      <>
-        {isOpen !== null && (
-          <Modal
-            open={isOpen !== null}
-            onClose={() => setIsOpen(13)}
-            sx={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              zIndex: 99999,
-            }}
-          >
-            <Stack
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs
               sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'grey.200',
-                borderRadius: '8px',
+                maxHeight: '10vh',
+                '.MuiTabs-flexContainer': {
+                  justifyContent: 'space-between',
+                },
+
+                '.MuiTab-root': {
+                  fontSize: '0.75rem',
+                  minWidth: '20px',
+                  flex: '1',
+                },
               }}
+              value={isOpen}
+              textColor="primary"
+              variant="scrollable"
+              scrollButtons="auto"
+              indicatorColor="primary"
+              onChange={handleChange}
+              aria-label="scrollable auto tabs example"
             >
-              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                {/* <Tabs value={isOpen} onChange={handleChange} aria-label="basic tabs example">
-                  <Tab label="Deposit" {...a11yProps(0)} />
-                  <Tab label="Withdraw" {...a11yProps(1)} />
-                </Tabs> */}
-              </Box>
-            </Stack>
-          </Modal>
-        )}
-      </>
-    </Stack>
+              <Tab label="Deposit" {...a11yProps(0)} />
+              <Tab label="Withdraw" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+        </Stack>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -12,66 +12,70 @@ function Profile() {
   const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
   const dispatch = useAppDispatch();
   const { data: details, isLoading } = useGetDetailsQuery(undefined);
-  console.log(details);
   return (
     <Container maxWidth="md" sx={{ py: 3 }}>
       <Stack gap={2}>
-        <Loading loading={isLoading} />
-        <Stack
-          direction={{
-            xs: 'column',
-            md: 'row',
-          }}
-          alignItems="center"
-          justifyContent="space-between"
-          gap={2}
-          sx={{
-            borderRadius: '8px',
-            overflow: 'hidden',
-          }}
-        >
-          <ProfileImage image={details?.image?.img} />
-          <Stack
-            gap={2}
-            sx={{
-              flex: 1,
-              width: '100%',
-              backgroundColor: 'Ink.Base',
-              p: 2,
-              borderRadius: '8px',
-            }}
-          >
-            {details?.walletAddress ? (
-              <TextBadgeCard
-                title="Wallet Address"
-                text={details?.walletAddress}
-                onClick={() => setIsWalletModalOpen(true)}
-              />
-            ) : (
-              <Button variant="contained" sx={{ height: 56 }} onClick={() => setIsWalletModalOpen(true)}>
-                {window?.ethereum?.chainId === import.meta.env.VITE_CHAIN_ID ? 'Connect Wallet' : 'Change Network'}
-              </Button>
-            )}
+        {isLoading ? (
+          <Loading loading={isLoading} />
+        ) : (
+          <>
+            <Stack
+              direction={{
+                xs: 'column',
+                md: 'row',
+              }}
+              alignItems="center"
+              justifyContent="space-between"
+              gap={2}
+              sx={{
+                borderRadius: '8px',
+                overflow: 'hidden',
+              }}
+            >
+              <ProfileImage image={details?.image?.img} />
+              <Stack
+                gap={2}
+                sx={{
+                  flex: 1,
+                  width: '100%',
+                  backgroundColor: 'Ink.Base',
+                  p: 2,
+                  borderRadius: '8px',
+                }}
+              >
+                {details?.walletAddress ? (
+                  <TextBadgeCard
+                    title="Wallet Address"
+                    text={details?.walletAddress}
+                    onClick={() => setIsWalletModalOpen(true)}
+                  />
+                ) : (
+                  <Button variant="contained" sx={{ height: 56 }} onClick={() => setIsWalletModalOpen(true)}>
+                    {window?.ethereum?.chainId === import.meta.env.VITE_CHAIN_ID ? 'Connect Wallet' : 'Change Network'}
+                  </Button>
+                )}
 
-            <Stack direction="row" gap={2}>
-              <TextBadgeCard title="Username" text={details?.username} />
-              <TextBadgeCard title="Money" text={details?.money} />
-            </Stack>
+                <Stack direction="row" gap={2}>
+                  <TextBadgeCard title="Username" text={details?.username} />
+                  <TextBadgeCard title="Money" text={details?.money} />
+                </Stack>
 
-            <Stack direction="row" gap={2}>
-              <TextBadgeCard title="Total Score" text={details?.totalScore} />
-              <TextBadgeCard title="Monthly Score" text={details?.monthlyScore} />
+                <Stack direction="row" gap={2}>
+                  <TextBadgeCard title="Total Score" text={details?.totalScore} />
+                  <TextBadgeCard title="Monthly Score" text={details?.monthlyScore} />
+                </Stack>
+                <Stack direction="row" gap={2}>
+                  <TextBadgeCard title="Wins" text={details?.wins} />
+                  <TextBadgeCard title="Draws" text={details?.draws} />
+                  <TextBadgeCard title="Losses" text={details?.losses} />
+                </Stack>
+              </Stack>
             </Stack>
-            <Stack direction="row" gap={2}>
-              <TextBadgeCard title="Wins" text={details?.wins} />
-              <TextBadgeCard title="Draws" text={details?.draws} />
-              <TextBadgeCard title="Losses" text={details?.losses} />
+            <Stack alignItems="center" justifyContent="center" sx={{ py: 2 }}>
+              <Link onClick={() => dispatch(logout())}>Log out</Link>
             </Stack>
-          </Stack>
-        </Stack>
-        <Stack alignItems="center" justifyContent="center" sx={{ py: 2 }}>
-          <Link onClick={() => dispatch(logout())}>Log out</Link>
-        </Stack>
+          </>
+        )}
       </Stack>
       <ConnectWallet isOpen={isWalletModalOpen} setIsOpen={setIsWalletModalOpen} />
     </Container>

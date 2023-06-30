f4615fccf78b30a8b86f3df8c12d14702b62f928
emre-cil <info.emrecil@gmail.com>
2023-06-30 18:29:24 +0300

    feat: add documentation and images

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,9 +8,7 @@ export default {
   networks: {
     matic: {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/oT188NmpOFYow2ixcuT6MKWs9lmbYjdS',
-      accounts: ['dbfa460768de88c6b3cdae4402e8d742ce0dda1b8771bccaffca5edaa6df6114'],
+      accounts: ['key'],
     },
   },
 };
-// 0x2269BE6442BFE7FF60Ff41dC457D629688676f00\
-// accounts: ['c481dff170f741e8bed796d5494cfb475059c6602afa39740022c869794e3f96'],
